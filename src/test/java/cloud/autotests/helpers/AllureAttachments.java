81ef308f3b882256f498b4f8a692d6beb46bf8d6
0yanna <yannaosodoeva@outlook.com>
2022-03-17 22:24:10 +0300

    edit attach

--- src/test/java/cloud/autotests/helpers/AllureAttachments.java
+++ src/test/java/cloud/autotests/helpers/AllureAttachments.java
@@ -1,40 +1,40 @@
 package cloud.autotests.helpers;
 
-import io.qameta.allure.Allure;
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
 
 public class AllureAttachments {
-    public static final Logger LOGGER = LoggerFactory.getLogger(AllureAttachments.class);
-
     @Attachment(value = "{attachName}", type = "text/plain")
-    private static String addMessage(String attachName, String text) {
-        return text;
+    public static String attachAsText(String attachName, String message) {
+        return message;
     }
 
-    public static void addBrowserConsoleLogs() {
-        addMessage("Browser console logs", DriverUtils.getConsoleLogs());
+    @Attachment(value = "Page source", type = "text/plain")
+    public static byte[] pageSource() {
+        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
     }
 
     @Attachment(value = "{attachName}", type = "image/png")
-    public static byte[] addScreenshotAs(String attachName) {
-        return DriverUtils.getScreenshotAsBytes();
+    public static byte[] screenshotAs(String attachName) {
+        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
-    @Attachment(value = "Page source", type = "text/html")
-    public static byte[] addPageSource() {
-        return DriverUtils.getPageSourceAsBytes();
+    public static void browserConsoleLogs() {
+        attachAsText(
+                "Browser console logs",
+                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+        );
     }
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
