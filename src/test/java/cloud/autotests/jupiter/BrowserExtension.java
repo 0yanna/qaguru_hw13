3789c0ddab29d30e94eccdb3b10decaab6bbde37
0yanna <yannaosodoeva@outlook.com>
2022-03-17 22:16:01 +0300

    edit attach

--- src/test/java/cloud/autotests/jupiter/BrowserExtension.java
+++ src/test/java/cloud/autotests/jupiter/BrowserExtension.java
@@ -1,14 +1,14 @@
-package cloud.autotests.jupiter;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-@Target(ElementType.METHOD)
-@Retention(RUNTIME)
-@ExtendWith(BrowserVideoExtension.class)
-public @interface BrowserExtension {
-}
+//package cloud.autotests.jupiter;
+//
+//import org.junit.jupiter.api.extension.ExtendWith;
+//
+//import java.lang.annotation.ElementType;
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.Target;
+//
+//import static java.lang.annotation.RetentionPolicy.RUNTIME;
+//@Target(ElementType.METHOD)
+//@Retention(RUNTIME)
+//@ExtendWith(BrowserVideoExtension.class)
+//public @interface BrowserExtension {
+//}
--- src/test/java/cloud/autotests/tests/SimpleVoisoTests.java
+++ src/test/java/cloud/autotests/tests/SimpleVoisoTests.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@BrowserExtension
+//@BrowserExtension
 public class SimpleVoisoTests extends TestBase {
     @Test
     @Description("Check if button 'request a demo' exists")
