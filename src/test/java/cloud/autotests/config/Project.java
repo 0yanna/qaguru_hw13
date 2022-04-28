4d71226a7a38b1443e91a5ca097fa435ae4dcca0
0yanna <yannaosodoeva@outlook.com>
2022-04-28 19:56:14 +0400

    refactoring

--- src/test/java/cloud/autotests/config/Project.java
+++ src/test/java/cloud/autotests/config/Project.java
@@ -5,4 +5,16 @@ import org.aeonbits.owner.ConfigFactory;
 public class Project {
     public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
 
+    public static boolean isWebMobile() {
+        return !config.browser().equals("");
+    }
+
+    public static boolean isRemoteWebDriver() {
+        return !config.remoteDriverUrl().equals("");
+    }
+
+    public static boolean isVideoOn() {
+        return !config.videoStorage().equals("");
+    }
+
 }
